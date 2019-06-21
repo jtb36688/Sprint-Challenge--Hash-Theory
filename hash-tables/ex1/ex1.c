5727990eb9111afd41f3845357c6a72e21d12d75
Jacob Bryan <jtb36688@gmail.com>
2019-06-21 12:01:59 -0400

    Started ex1 by looping over weights array.

--- hash-tables/ex1/ex1.c
+++ hash-tables/ex1/ex1.c
@@ -7,7 +7,9 @@ Answer *get_indices_of_item_weights(int *weights, int length, int limit)
 {
   HashTable *ht = create_hash_table(16);
 
-  /* YOUR CODE HERE */
+  for (int = 0; i < length, i++) {
+    hash_table_retrieve(ht, weights[i]);
+  }
 
   return NULL;
 }
